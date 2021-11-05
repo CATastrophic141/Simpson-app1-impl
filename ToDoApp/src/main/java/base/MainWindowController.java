203808604a4b7c7480289e989b23bc918667e2e0
Rylan S <rylan.simpson.02@gmail.com>
2021-11-04 23:27:01 -0400

    Added implementation of adding and removing items from table

--- ToDoApp/src/main/java/base/MainWindowController.java
+++ ToDoApp/src/main/java/base/MainWindowController.java
@@ -5,27 +5,40 @@
 
 package base;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.CheckBoxTableCell;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
+import java.net.URL;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class MainWindowController{
+public class MainWindowController implements Initializable {
 
     /*
     * Try preloading all scenes, and swap between scenes
    */
 
     //Storage variables
-    ArrayList<ToDoItem> listOfItems;
-    ToDoItem currentItem;
-    boolean showCompleted = true;
-    boolean showIncomplete = true;
+    AppResource resources = new AppResource();
 
     @FXML
     private MenuItem saveBtn;
@@ -71,8 +84,8 @@ public class MainWindowController{
     @FXML
     private void setHideCompletedBtn(ActionEvent event){
         //Change hide completion status
-        showCompleted = !showCompleted;
-        System.out.printf("Set showCompleted status to %b%n", showCompleted);
+        resources.showCompleted = !resources.showCompleted;
+        System.out.printf("Set showCompleted status to %b%n", resources.showCompleted);
     }
 
     @FXML
@@ -81,8 +94,8 @@ public class MainWindowController{
     @FXML
     private void setHideIncompleteBtn(ActionEvent event){
         //Change hide completion status
-        showIncomplete = !showIncomplete;
-        System.out.printf("Set showIncomplete status to %b%n", showIncomplete);
+        resources.showIncomplete = !resources.showIncomplete;
+        System.out.printf("Set showIncomplete status to %b%n", resources.showIncomplete);
     }
 
     @FXML
@@ -107,17 +120,109 @@ public class MainWindowController{
     private Button itemRemoveBtn;
 
     @FXML
-    private TableView<?> itemTable;
+    private Button clearAllItemsBtn;
 
     @FXML
-    void useListOfItemsAddBtn(ActionEvent event) {
-        //Take information from data boxes and add item to table
+    private TableView<ToDoItem> itemTable;
+    @FXML
+    private TableColumn<ToDoItem, CheckBox> completionColumn;
+    @FXML
+    private TableColumn<ToDoItem, String> nameColumn;
+    @FXML
+    private TableColumn<ToDoItem, LocalDate> dateColumn;
+    @FXML
+    private TableColumn<ToDoItem, String> detailColumn;
 
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Set up columns for custom use
+
+        completionColumn.setCellValueFactory(new PropertyValueFactory<>("CompletionStatus"));
+
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("itemName"));
+        nameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        nameColumn.setOnEditCommit(
+                t -> (t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())
+                ).setItemName(t.getNewValue())
+        );
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY-MM-DD");
+        dateColumn.setCellValueFactory(new PropertyValueFactory<>("itemDueDate"));
+
+
+        detailColumn.setCellValueFactory(new PropertyValueFactory<>("itemDetails"));
+        detailColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        detailColumn.setOnEditCommit(
+                t -> (t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())
+                ).setItemDetails(t.getNewValue())
+        );
+
+
+        itemTable.setItems(getItemList());
+    }
+
+    public ObservableList<ToDoItem> getItemList() {
+        resources.items.add(new ToDoItem("Example", LocalDate.of(2000, Month.JANUARY, 1), "This is an example item"));
+        return resources.items;
     }
 
     @FXML
-    void useListOfItemsRmvBtn(ActionEvent event) {
-        //Remove item from table
+    public void addItemToList(ActionEvent event) {
+        String name;
+        LocalDate date;
+        String details;
+        boolean isCompleted;
+
+        //Save name if it is valid
+        if (itemNameBox.getText().length() < 1 || itemNameBox.getText().length() > 256) {
+            errorLabel.setText("Please enter a valid name for the item. ");
+            return;
+        } else {
+            name = itemNameBox.getText();
+        }
+
+        //Save details if they are valid
+        if (itemDetailBox.getText().length() < 1 || itemDetailBox.getText().length() > 256) {
+            errorLabel.setText("Please enter valid details for the item. ");
+            return;
+        }
+        else {
+            details = itemDetailBox.getText();
+        }
+
+        //Save date, invalid values are ignored
+        date = itemDateBox.getValue();
+
+        //Get completion status
+        isCompleted = completionStatusBtn.isSelected();
+
+        ToDoItem item = new ToDoItem(name, date, details, isCompleted);
+        resources.items.add(item);
+        itemNameBox.clear();
+        itemDateBox.getEditor().clear();
+        itemDateBox.setValue(null);
+        itemDetailBox.clear();
+        completionStatusBtn.setSelected(false);
     }
 
+    @FXML
+    public void deleteItemFromList(ActionEvent event) {
+            int selectionIndex = itemTable.getSelectionModel().getSelectedIndex();
+            resources.items.remove(selectionIndex);
+    }
+
+    @FXML
+    void clearList(ActionEvent event) {
+        itemTable.getItems().clear();
+    }
+
+}
+
+
+ class AppResource {
+     ObservableList<ToDoItem> items = FXCollections.observableArrayList();
+     boolean showCompleted = true;
+     boolean showIncomplete = true;
 }
--- ToDoApp/src/main/java/base/ToDoItem.java
+++ ToDoApp/src/main/java/base/ToDoItem.java
@@ -1,22 +1,66 @@
 package base;
 
-class ToDoItem {
-     String itemName;
-     String itemDueDate;
-     String itemDetails;
-    boolean isCompleteStatus;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.scene.control.CheckBox;
+import java.time.LocalDate;
 
-    public ToDoItem(String name, String date, String details) {
+public class ToDoItem {
+    private SimpleStringProperty itemName;
+    private LocalDate itemDueDate;
+    private SimpleStringProperty itemDetails;
+    private CheckBox completionStatus;
+
+    public ToDoItem(String name, LocalDate date, String details) {
         //Set instance details to passed arguments
-        itemName = name;
+        itemName = new SimpleStringProperty(name);
         itemDueDate = date;
-        itemDetails = details;
-        isCompleteStatus = false;
+        itemDetails = new SimpleStringProperty(details);
+        completionStatus = new CheckBox();
+        completionStatus.setSelected(false);
+    }
+    public ToDoItem(String name, LocalDate date, String details, boolean selection) {
+        //Set instance details to passed arguments
+        itemName = new SimpleStringProperty(name);
+        itemDueDate = date;
+        itemDetails = new SimpleStringProperty(details);
+        completionStatus = new CheckBox();
+        completionStatus.setSelected(selection);
     }
 
-    public void setCompleteStatus(){
-        //Set item complete status to true
-        isCompleteStatus = !isCompleteStatus;
+    public String getItemName(){
+        return itemName.get();
+    }
+
+    public void setItemName(String name) {
+        itemName.set(name);
+    }
+
+    public LocalDate getItemDueDate(){
+        return itemDueDate;
+    }
+
+    public String getItemDueDateAsString(){
+        return itemDueDate.toString();
+    }
+
+    public void setItemDueDate(LocalDate date) {
+        itemDueDate = date;
+    }
+
+    public String getItemDetails(){
+        return itemDetails.get();
+    }
+
+    public void setItemDetails(String details) {
+        itemDetails.set(details);
+    }
+
+    public CheckBox getCompletionStatus(){
+        return completionStatus;
+    }
+
+    public void setCompletionStatus(CheckBox selection) {
+        completionStatus = selection;
     }
 }
 
